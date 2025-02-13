23645ef502cda1e2791a4f944bcdbd6774c0748e
ibcho <ibrawokring@gmail.com>
2025-02-13 11:45:49 +0200

    adding radiobuttons tests and assertions

---
+++ tests/radioButtons.spec.ts
@@ -0,0 +1,59 @@
+import { test, expect, BrowserContext, Page } from '@playwright/test';
+import RadioButtonsPage from '../pages/RadioButtonsPage';
+import ElementsPage from '../pages/ElementsPage';
+import HomePage from '../pages/HomePage';
+
+test.describe('Radio Buttons Tests', () => {
+    let radioButtonsPage: RadioButtonsPage;
+    let homePage: HomePage;
+    let elementsPage: ElementsPage;
+    let context: BrowserContext;
+    let page: Page;
+
+    test.beforeAll(async ({ browser }) => {
+        context = await browser.newContext();
+        page = await context.newPage();
+
+        homePage = new HomePage(page);
+        elementsPage = new ElementsPage(page);
+        radioButtonsPage = new RadioButtonsPage(page);
+
+        await homePage.navigateToHomePage();
+        await homePage.gotoElements();
+        await elementsPage.navigateToRadioButton();
+    });
+
+    test.afterAll(async () => {
+        await context.close();
+    });
+
+    test('is Radio button title is visible', async () => {
+        expect(await radioButtonsPage.isRadioButtonsTitleVisible()).toBeTruthy();
+    });
+
+    test('select "yes"', async () => {
+        await radioButtonsPage.selectRadioButton('yes');
+        expect(await radioButtonsPage.isRadioButtonSelected('yes')).toBeTruthy();
+        expect(await radioButtonsPage.getSelectedRadioButtonText()).toBe('Yes');
+    });
+
+    test('select "impressive"', async () => {
+        await radioButtonsPage.selectRadioButton('impressive');
+        expect(await radioButtonsPage.isRadioButtonSelected('impressive')).toBeTruthy();
+        expect(await radioButtonsPage.getSelectedRadioButtonText()).toBe('Impressive');
+    });
+
+    test('should not select the "yes" radio button when "impressive" is selected', async () => {
+        await radioButtonsPage.selectRadioButton('impressive');
+        expect(await radioButtonsPage.isRadioButtonSelected('yes')).toBeFalsy();
+    });
+
+    test('should not select the "impressive" radio button when "yes" is selected', async () => {
+        await radioButtonsPage.selectRadioButton('yes');
+        expect(await radioButtonsPage.isRadioButtonSelected('impressive')).toBeFalsy();
+    });
+
+    test('should verify that "no" radio button is disabled', async () => {
+        expect(await radioButtonsPage.isRadioButtonDisabled('no')).toBeTruthy();
+    });
+});
